5e4815dadf46644e88b4b2430655d8733da5a2c5
AgustinRH <surviiplay@gmail.com>
2024-12-15 14:23:50 +0100

    final

--- PracticaObligatoria/Blackjack.java
+++ PracticaObligatoria/Blackjack.java
@@ -3,31 +3,35 @@ package PracticaObligatoria;
 import java.util.Scanner;
 
 public class Blackjack {
-   
+    // Variables de la clase
     private Baraja b;
     private String jugador;
     private double puntosJugador;
     private double puntosCroupier;
 
+    // Constructor de Blackjack
     public Blackjack(int tipo){
             b = new Baraja(tipo);
             b.barajar();
     }
 
+    // Método para jugar
     public void jugar() {
         Scanner sc = new Scanner(System.in);
+
 
         System.out.println("Bienvenido al juego de Blackjack. \n Introducción al juego: Para ganar debes obtener 21 puntos, si te pasas pierdes, si el croupier obtiene más puntos que tú, pierdes.");
         
         System.out.println("Introduce tu nombre: ");
         jugador = sc.next();
 
-        jugarTurnoJugador();
-        jugarTurnoCroupier();
+        jugarTurnoJugador();  // Método para jugar el turno del jugador
+        jugarTurnoCroupier(); // Método para jugar el turno del croupier
 
-        determinarGanador();
+        determinarGanador(); // Método para determinar el ganador
     }
 
+        // Método para jugar turno del jugador
     private void jugarTurnoJugador() {
         Scanner sc = new Scanner(System.in);
         boolean stop = false;
@@ -43,12 +47,13 @@ public class Blackjack {
             }
         }
 
-        // Si el jugador saca un As, le pregunta si quiere que valga 1 o 11
+        // Si el jugador se pasa de 21, pierde
         if (puntosJugador > 21) {
             System.out.println("Te has pasado de 21 puntos, has perdido.");
             return;
         }
 
+        // Si no se pasa de 21 puede elegir entre plantarse o sacar otra carta 
         while (!stop && puntosJugador < 21) {
             System.out.println("PUNTUACIÓN: " + puntosJugador);
             System.out.println("¿Quieres sacar otra carta? (s/n)");
@@ -70,16 +75,20 @@ public class Blackjack {
         }
     }
 
+    // Método para jugar turno del Croupier
     private void jugarTurnoCroupier() {
-        // El croupier saca cartas hasta alcanzar al menos 17 puntos
+        
+        // Inicializamos los puntos del croupier a 0
         puntosCroupier = 0;
 
+        // El croupier saca cartas hasta alcanzar al menos 17 puntos    
         while (puntosCroupier < 17) {
             Carta c = b.siguiente();
             puntosCroupier += c.getValor();
             System.out.println("El croupier ha sacado un " + c);
         }
 
+        // Comprobamos si el croupier se ha pasado de puntos o no.
         if (puntosCroupier > 21) {
             System.out.println("El croupier se ha pasado de 21 puntos.");
         } else {
@@ -87,6 +96,7 @@ public class Blackjack {
         }
     }
 
+    // Método para determinar el ganador
     private void determinarGanador() {
         System.out.println("\nResultados finales:");
         System.out.println(jugador + " tiene " + puntosJugador + " puntos.");
